8f855e8a60dfb90448bed11b6a165f74316b09ba
Madhusuthanan B <Madhusuthanan B>
2017-07-21 04:06:19 +0530

    Trying out material design

--- ReusableModules/src/app/app.component.ts
+++ ReusableModules/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  states = [{code: 'AL', name: 'Alabama'}, {code: 'TX', name: 'Texas'}, {code: 'SF', name: 'San francisco'}];
 }
