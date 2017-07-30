bd7f34ffc7a700a14ffec5603229e7486bce1b7f
Madhusuthanan <madhub081011@gmail.com>
2017-07-31 00:26:25 +0530

    Including chart.js in the project

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() {
-    console.log('Home component');
-   }
+  }
 
   ngOnInit() {
+    const canvas = document.getElementById('bar-chart-grouped');
+    const chart = new Chart(canvas, {
+      type: 'bar',
+      data: {
+        labels: ['2013', '2014', '2016', '2017'],
+        datasets: [
+          {
+            label: 'Angular',
+            backgroundColor: '#3e95cd',
+            data: [25, 40, 60, 85]
+          }, {
+            label: 'Backbone',
+            backgroundColor: '#8e5ea2',
+            data: [80, 60, 47, 15]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Angular vs Backbone growth (Percentage)'
+        }
+      }
+    });
   }
 
 }
